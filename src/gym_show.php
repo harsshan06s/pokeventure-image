f613264c57d0cf074f62d33588964e7c444d9e16
Harsshan Senthil <ryzenanimation@gmail.com>
2025-11-14 13:49:15 +0530

    Refactor image handling in gym_show.php to use default image for empty slots and improve code clarity

--- src/gym_show.php
+++ src/gym_show.php
@@ -44,32 +44,26 @@ $imagine = new Imagine\Gd\Imagine();
 $canvasWidth = 518;
 $canvasHeight = 288;
 
-$image = $imagine->create(new Box(518, 288));
+$image = $imagine->create(new Box($canvasWidth, $canvasHeight));
 
 $bg = $imagine->open('./img/bgs/-1.jpeg');
 $pokemonImages = [];
 $totalWidth = 0;
 for($i = 0; $i < count($data->pokemons); $i++) {
     try {
-    $pkmImg = $imagine->open('./img/front' . ($data->pokemons[$i]->shiny ? '-shiny' : '') . '/' . normalizeName($data->pokemons[$i]->name) . normalizeName($data->pokemons[$i]->forme) . '.gif');
-    $pokemonImages[] = $pkmImg;
-    $totalWidth += $pkmImg->getSize()->getWidth();
+        if ($data->pokemons[$i]->name === "?") {
+            // Use a default image for empty slots
+            $pkmImg = $imagine->open('./img/missingno.png');
+        } else {
+            $pkmImg = $imagine->open('./img/front' . ($data->pokemons[$i]->shiny ? '-shiny' : '') . '/' . normalizeName($data->pokemons[$i]->name) . normalizeName($data->pokemons[$i]->forme) . '.gif');
+        }
+        $pokemonImages[] = $pkmImg;
+        $totalWidth += $pkmImg->getSize()->getWidth();
     } catch(Exception $e) {
         echo $e;
         die;
     }
 }
-/*try {
-    $enemy_pokemon = $imagine->open('./img/front' . ($data->p2->shiny ? '-shiny' : '') . '/' . normalizeName($data->p2->name) . $data->p2->forme . '.gif');
-} catch (Exception $e) {
-    $enemy_pokemon = $imagine->open('./img/missingno.png');
-}
-$enemy_pokemon_size = $enemy_pokemon->getSize();
-try {
-    $player_pokemon = $imagine->open('./img/back' . ($data->p1->shiny ? '-shiny' : '') . '/' . normalizeName($data->p1->name) . $data->p1->forme . '.gif');
-} catch (Exception $e) {
-    $player_pokemon = $imagine->open('./img/missingno.png');
-}*/
 
 $palette = new RGB();
 $font = new Font('./img/pokemon_fire_red.ttf', 18, $palette->color('#000'));
@@ -82,14 +76,6 @@ for($i = 0; $i < count($pokemonImages); $i++) {
     $image->draw()->text(strtoupper($data->pokemons[$i]->name), $font, new Point($i * 150 - $nameBox->getWidth() / 2 + 110, 180));
     $image->draw()->text('Lv.'. ($data->pokemons[$i]->level), $font, new Point($i * 150 - $nameBox->getWidth() / 2 + 122, 200));
 }
-
-/*$image->paste($enemy_pokemon, new Point($pos_x_enemy, $pos_y_enemy));
-$image->paste($player_pokemon, new Point($pos_x_player, $pos_y_player));
-
-$image->draw()->text(strtoupper($nameEnemy), $font, new Point(31, 28));
-$image->draw()->text('Lv.' . $data->p2->level, $font, new Point(155, 28), 0, 200);
-$image->draw()->text(strtoupper($namePlayer), $font, new Point(310, $canvasHeight - 95));
-$image->draw()->text('Lv.' . $data->p1->level, $font, new Point(430, $canvasHeight - 95));*/
 
 $options = array(
     'jpeg_quality' => 90,
